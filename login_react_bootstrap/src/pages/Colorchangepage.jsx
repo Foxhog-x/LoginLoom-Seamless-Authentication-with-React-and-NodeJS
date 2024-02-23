233e7b9e9a73faf0c012abca2a63c415d6e17430
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2024-02-23 20:36:39 +0530

    updated colorchangepage project

--- login_react_bootstrap/src/pages/Colorchangepage.jsx
+++ login_react_bootstrap/src/pages/Colorchangepage.jsx
@@ -1,23 +1,84 @@
+import { useState } from "react";
+
 export const Colorchangepage = () => {
-  let rgbArray = [];
-  const rgbColorChange = () => {
-    rgbArray.splice(0, rgbArray.length);
-    for (let i = 0; i < 3; i++) {
-      const randomNo = Math.random(0, 10);
-      rgbArray.push(Math.floor(randomNo * 255));
+  const [showColorType, setShowColorType] = useState("");
+  const hexColor = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  let allRgbArray = [];
+
+  const checkArray = () => {
+    if (typeof allRgbArray === "object") {
+      allRgbArray.splice(0, allRgbArray.length);
+    } else {
+      allRgbArray = [];
     }
-
-    settingColor(rgbArray);
-  };
-  const settingColor = (...rgbArray) => {
-    document.body.style = `background: rgb(${rgbArray.toString()})`;
   };
 
+  const rgbColorChange = (colorType) => {
+    switch (colorType) {
+      case "rgb":
+        checkArray();
+        for (let i = 0; i < 3; i++) {
+          const randomNo = Math.random(0, 10);
+          allRgbArray.push(Math.floor(randomNo * 255));
+        }
+        settingRgbColor("rgb", allRgbArray);
+        break;
+
+      case "hex":
+        checkArray();
+        for (let i = 0; i < 6; i++) {
+          allRgbArray.push(hexColor[Math.floor(Math.random(0, 10) * 15)]);
+        }
+        allRgbArray = "#" + allRgbArray.join("").toString();
+        settingRgbColor("hex", allRgbArray);
+
+        break;
+      default:
+        break;
+    }
+  };
+  const settingRgbColor = (colorType, ...allRgbArray) => {
+    switch (colorType) {
+      case "rgb":
+        setShowColorType(`rgb ${allRgbArray.toString()}`);
+        document.body.style = `background: rgb(${allRgbArray.toString()})`;
+        break;
+
+      case "hex":
+        setShowColorType(`hex ${allRgbArray}`);
+        document.body.style = `background: ${allRgbArray}`;
+
+        break;
+      default:
+        break;
+    }
+  };
+  console.log(showColorType);
   return (
-    <div style={{ display: "flex", gap: "35px", justifyContent: "center" }}>
-      <button onClick={() => rgbColorChange()}>RGB color</button>
-      <button>Random color</button>
-      <button>HEX Color</button>
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          gap: "35px",
+          justifyContent: "center",
+          padding: "12px",
+          marginTop: "20px",
+        }}
+      >
+        <button onClick={() => rgbColorChange("rgb")}>RGB color</button>
+        <button onClick={() => rgbColorChange("hex")}>Hex color</button>
+        <button>Color</button>
+      </div>
+      <div></div>
+      <div
+        style={{
+          height: "80vh",
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <h1>{showColorType}</h1>
+      </div>
+    </>
   );
 };
